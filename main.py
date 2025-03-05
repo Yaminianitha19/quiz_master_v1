c70c6ab61a6ba07e43077ca72e901af7ba124e74
Yaminianitha19 <yaminianitha22@gmail.com>
2025-03-05 18:40:13 +0530

    default admin added

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # the above line is to suppress a warning, not mandatory
 db.init_app(app)
 
+def create_admin():
+    admin = User.query.filter_by(role='admin').first()
+    if not admin:
+        admin = User(email='admin@quiz.com', password='password', full_name='Admin', role='admin')
+        db.session.add(admin)
+        db.session.commit()
 
 
 @app.route('/')
@@ -65,6 +71,8 @@ def admin_dashboard():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        create_admin()
+
     
     app.run(debug=True)
     
